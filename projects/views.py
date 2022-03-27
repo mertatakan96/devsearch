d2c6a44411e687a596832597598ba706da7d6d54
Mert Atakan Duduoğlu <atakanduduoglu@gmail.com>
2022-03-27 16:38:24 +0300

    template edits

--- projects/views.py
+++ projects/views.py
@@ -1,36 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-projectsList = [
-    {
-        'id': '1',
-        'title': 'Ecommerce Website',
-        'description': 'Fully functional ecommerce website'
-    },
-    {
-        'id': '2',
-        'title': 'Portfolio Website',
-        'description': 'A personal website to write articles and display work'
-    },
-    {
-        'id': '3',
-        'title': 'Social Network',
-        'description': 'An open source project built by the community'
-    }
-]
+from .models import Project
 
 
 def projects(request):
-    msg = 'Hello, you are on projects page'
-    context = {'projects': projectsList}
+    projects = Project.objects.all()
+    context = {'projects': projects}
     return render(request, 'projects/projects.html', context)
 
 
 def project(request, pk):
-    projectObj = None
-    for i in projectsList:
-        if i['id'] == pk:
-            projectObj = i
+    projectObj = Project.objects.get(id=pk)
     return render(request, 'projects/single-project.html', {'project': projectObj})
 
